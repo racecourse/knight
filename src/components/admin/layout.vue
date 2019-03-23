aad4693e1a324fdd55f03174cccaf9172308e5c9
shisang <shisang@fordeal.com>
2019-03-23 14:06:18 +0800

    2

---
+++ src/components/admin/layout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="layout">
+    <div class="header">
+      <SideNav></SideNav>
+    </div>
+    <div class="main-wrapper">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+<style>
+@import './main.css';
+</style>
+<script>
+import SideNav from '../nav/index.vue';
+// import Bottom from '../common/bottom.vue';
+export default {
+  components: {
+    SideNav,
+    // Bottom,
+  },
+}
+</script>
